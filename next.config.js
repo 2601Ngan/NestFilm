4aa40ecdacc9031cfd64ed659f0113d6225ee80b
TeriSimp <154432286+TeriSimp@users.noreply.github.com>
2025-03-19 15:14:46 +0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+
+/** @type {import("next").NextConfig} */
+const config = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'image.tmdb.org',
+                pathname: '/t/p/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.themoviedb.org',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com',
+                pathname: '/**',
+            },
+        ],
+    },
+};
+
+export default config;
