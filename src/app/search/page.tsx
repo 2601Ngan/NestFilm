1392178a72a352adced91aec28df31ea38c9aa8b
TeriSimp <154432286+TeriSimp@users.noreply.github.com>
2025-03-25 20:50:58 +0700

    Update page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,7 @@
 import SearchClient from "./Search";
 
-export default function Search({ searchParams }: { searchParams: { q: string } }) {
-  return <SearchClient query={searchParams?.q} />;
+export default async function Search(props: { searchParams: Promise<{ q: string; with_keywords: string }> }) {
+  const searchParams = await props.searchParams;
+  
+  return <SearchClient query={searchParams.q} withKeywords={searchParams.with_keywords} />;
 }
